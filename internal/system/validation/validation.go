6b426b45fe2f92d99894269353163745b43f1412
OKM <okm@neudp.pro>
2024-08-20 20:50:29 +0300

    add optional validation, refactor

--- internal/system/validation/validation.go
+++ internal/system/validation/validation.go
@@ -2,14 +2,33 @@ package validation
 
 type Validator func(value any) []error
 
-func All(value any, rules ...Validator) []error {
-	errs := make([]error, 0)
+func All(rules ...Validator) Validator {
+	return func(value any) []error {
+		errs := make([]error, 0)
 
-	for _, rule := range rules {
-		if err := rule(value); err != nil {
-			errs = append(errs, err...)
+		for _, rule := range rules {
+			if err := rule(value); err != nil {
+				errs = append(errs, err...)
+			}
 		}
-	}
 
-	return errs
+		return errs
+	}
+}
+
+func Optional(rules ...Validator) Validator {
+	return func(value any) []error {
+		ptrValue, ok := value.(*any)
+
+		checkedValue := value
+		if ok {
+			if ptrValue == nil {
+				return nil
+			} else {
+				checkedValue = *ptrValue
+			}
+		}
+
+		return All(rules...)(checkedValue)
+	}
 }
